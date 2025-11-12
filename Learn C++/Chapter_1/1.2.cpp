2b2ecd9cfb57a040208eb3ef60682470008a4aa0
Mcloyf <alexortiz242005@gmail.com>
2025-11-11 22:57:11 -0600

    Add content to 1.2.cpp
    Mostly comment as it's what the topic is about, pretty boring as it's mostly comments (duh)

--- Learn C++/Chapter_1/1.2.cpp
+++ Learn C++/Chapter_1/1.2.cpp
@@ -2,6 +2,26 @@
 
 #include <iostream>
 
+
+/* This is a multi-line comment
+   This is fairly easy to grasp*/
+
+/* This looks a little nicer
+ * And it doesn't effect the comment
+ * it can be easier to read 
+ */
+
+// Comment should be used at the top of a program to tell what it does
+
+// Multi-line comments can be used to describe exactly how a program works
+// useful in a team so you don't have to look line-by-line how a program works
+
+/*Comments should be used to describe why the code is doing something. 
+ *A bad statement comment explains WHAT the code is doing. 
+ *If you ever write code that is so complex that needs a comment to explain what a statement
+ *is doing, you probably need to rewrite your statement, not comment it.
+ */
+
 void singleLine()
 {
     std::cout << "Hello world!\n"; // std::cout lives in the iostream library
@@ -22,11 +42,30 @@ void singleLine()
     std::cout << "Yeah!\n";
 }
 
-
-
-
 int main()
 {
+    // Set sight range to 0
+    int sight = 0;          // Bad comment
+
+    // The player just drank a potion of blindness and can not see anything
+    sight = 0;              // Good commit, tells us why the player's sight to set to 0
     singleLine();
     return 0;
 }
+
+//Good comments
+//________________________________________________________________________
+// We decided to use a linked list instead of an array because
+// arrays do insertion too slowly.
+//________________________________________________________________________
+// We're going to use Newton's method to find the root of a number because
+// there is no deterministic way to solve these equations.
+//________________________________________________________________________
+
+//Best Practice
+//Comment your code liberally, and write your comments as if speaking to someone
+//who has no idea what the code does. Don’t assume you’ll remember why you made specific choices.
+
+// Documentation generation programs like Doxygen can be useful
+
+//For VSCode you can comment a selection with crtl-/
