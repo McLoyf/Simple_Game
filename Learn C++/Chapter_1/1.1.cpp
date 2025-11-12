3d13803aaf70cb00f0351517d7d0710f3d150857
Mcloyf <alexortiz242005@gmail.com>
2025-11-11 22:11:59 -0600

    Add comments to explain each part of hello world program

--- Learn C++/Chapter_1/1.1.cpp
+++ Learn C++/Chapter_1/1.1.cpp
@@ -1,7 +1,9 @@
+// Including iostream library in the C++ standard library
 #include <iostream>
 
 int main()
 {
+    // character output = cout
     std::cout << "Hello world!";
-    return 0;
+    return 0;// return 0 to tell that it run successfully as C++ doesn't do this automatically like other langauges
 }
