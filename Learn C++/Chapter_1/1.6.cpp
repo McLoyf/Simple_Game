4c14e927e51a3e77078fa9c1476433ca41d53c6c
McLoyf <alex.ortiz242005@gmail.com>
2026-01-01 17:10:09 -0600

    Finish lesson 1.6
    Not much written as I already understand the material

--- Learn C++/Chapter_1/1.6.cpp
+++ Learn C++/Chapter_1/1.6.cpp
@@ -1,1 +1,12 @@
 // Uninitialized variables and undefined behavior
+
+#include <iostream>
+
+int main()
+{
+    int x;              // define x but leave it uninitialized
+
+    std::cout << x;     // print the value of x (who knows what you'll get)
+
+    return 0;
+}
