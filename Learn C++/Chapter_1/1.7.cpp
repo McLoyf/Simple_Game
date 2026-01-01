e30c3e268a1279855fc24363b0444145b52783fb
McLoyf <alex.ortiz242005@gmail.com>
2026-01-01 17:44:27 -0600

    Finish lesson 1.7

--- Learn C++/Chapter_1/1.7.cpp
+++ Learn C++/Chapter_1/1.7.cpp
@@ -1,1 +1,17 @@
 // Keywords and naming identifiers
+
+main()
+{
+    int exampleVariable;
+    double PI{ 3.1415 };
+
+    return 0;
+}
+
+/* About naming variables
+*  This lesson has nothing I don't already know, seeing as this isn't my first programming
+*  langauge. The same things apply here, you can't use keywords as a variable name and the
+*  name should be specific. variable names can't start with numbers, should not have whitespace
+*  and shouldn't be all caps unless it's a true constant. Example of a true constant, double 
+*  PI = 3.1415
+*/
