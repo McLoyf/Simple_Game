6f4253d29eeac98e01104d9c928be92ba5859ee7
McLoyf <alex.ortiz242005@gmail.com>
2026-01-02 17:27:07 -0600

    Finish 1.11
    The lesson says to not just jump into coding a solution which makes sense but the program is so easy to make that I ignored that. Doesn't really matter for this lesson.

--- Learn C++/Chapter_1/1.11.cpp
+++ Learn C++/Chapter_1/1.11.cpp
@@ -1,1 +1,16 @@
 // Developing your first program
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    
+    int x;
+    std::cin >> x;
+
+    std::cout << "Double " << x << " is: " << x * 2 << '\n';
+    std::cout << "Triple " << x << " is: " << x * 3 << '\n';
+
+    return 0;
+}
