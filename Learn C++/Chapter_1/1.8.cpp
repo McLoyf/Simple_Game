9f2fa5bf29cbc54d2727098f35f68cd9cc6b7484
McLoyf <alex.ortiz242005@gmail.com>
2026-01-01 18:38:09 -0600

    Finish 1.8

--- Learn C++/Chapter_1/1.8.cpp
+++ Learn C++/Chapter_1/1.8.cpp
@@ -1,1 +1,29 @@
 // Whitespace and basic formatting
+
+#include <iostream> 
+int main(){std::cout << "Hello, World \n"; std::string name; std::cout << "Enter your name: "; std::cin >> name; std::cout << "Hello " << name; return 0;}
+
+/* Formatting
+*  Basically just write code in a way that makes the code easy to read but their
+*  isn't just one way to write code for readiabilty, just don't do what the above
+*  code does.
+*
+*  It's really hard to read, right?
+*
+*  Below is the same code made easy to read:
+*
+*  #include <iostream>
+*
+*  int main()
+*  {
+*       std::string name;
+*
+*       std::cout << "Hello, World \n";
+*       std::cout << "Enter your name ";
+*
+*       std::cin >> name;
+*       std::cout << "Hello " << name;
+*
+*       return 0;
+*  }
+*/
