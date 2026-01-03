8c35694b12557615cbaeca3211feaa2aaaefba04
McLoyf <alex.ortiz242005@gmail.com>
2026-01-03 16:32:00 -0600

    Complete 2.1 and Create 2.2

--- Learn C++/Chapter_2/2.1.cpp
+++ Learn C++/Chapter_2/2.1.cpp
@@ -1,1 +1,36 @@
 // Introduction to functions
+
+#include <iostream>
+
+void doB()
+{
+    std::cout << "In doB()\n";
+}
+
+void doA()
+{
+    std::cout << "Starting doA()\n";
+
+    doB();
+
+    std::cout << "Ending doA()\n";
+}
+
+int main()
+{
+    std::cout << "Starting main()\n";
+
+    doA();
+
+    std::cout << "Ending main()\n";
+
+    return 0;
+}
+
+/* Output
+* Starting main()
+* Starting doA()
+* In doB()
+* Ending doA()
+* Ending main()
+*/
