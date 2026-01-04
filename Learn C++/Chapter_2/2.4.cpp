e668e2f8a9dc35369f5e70aebfe53317a623f18b
McLoyf <alex.ortiz242005@gmail.com>
2026-01-03 19:44:29 -0600

    Complete 2.4

--- Learn C++/Chapter_2/2.4.cpp
+++ Learn C++/Chapter_2/2.4.cpp
@@ -1,1 +1,26 @@
 // Introduction to function parameters and arguments
+
+#include <iostream>
+
+int doubleNumber(int x)
+{
+    return 2 * x;
+}
+
+int getValueFromUser()
+{
+    std::cout << "Enter an integer: ";
+    int input;
+    std::cin >> input;
+
+    return input;
+}
+
+int main()
+{
+    std::cout << doubleNumber(getValueFromUser());
+
+    return 0;
+}
+
+// My Question 4 and 5 answer above
