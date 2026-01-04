ca5853360d1efb9ecb4b8e35c241fc445d1f5960
McLoyf <alex.ortiz242005@gmail.com>
2026-01-04 15:15:50 -0600

    Complete 2.8
    Add "add.cpp" file to 2.8 folder

--- Learn C++/Chapter_2/2.8/2.8.cpp
+++ Learn C++/Chapter_2/2.8/2.8.cpp
@@ -1,1 +1,17 @@
 // Programs with multiple code files
+
+#include <iostream>
+
+int add(int x, int y);
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3,4) << '\n';
+    return 0;
+}
+
+/* I go into a bash terminal and input "g++ 2.8.cpp add.cpp" and run the resulting a.exe. I chose to
+   rename it and go into the terminal and input ./{filename}.exe and it runs fine.
+   "#include add.cpp" also works but apparently it's not recommended as it increases the risk
+   of naming conflicts and other anticipated consequences
+*/
