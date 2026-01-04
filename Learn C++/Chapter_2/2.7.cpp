16ad7441ed44d39401444a8eea4b379db4d3b2d2
McLoyf <alex.ortiz242005@gmail.com>
2026-01-04 14:38:36 -0600

    Complete 2.7

--- Learn C++/Chapter_2/2.7.cpp
+++ Learn C++/Chapter_2/2.7.cpp
@@ -1,1 +1,23 @@
 // Forward declarations and definitions
+
+#include <iostream>
+
+int add(int x, int y);      // Forward declaration of add()
+int doMath(int first, int second, int third, int fourth);
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3,4) << '\n'; // Without forward declaration this will break here
+    std::cout << "Here's some math " << doMath(4, 6, 8, 12) << '\n';
+    return 0;
+}
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int doMath(int first, int second, int third, int fourth)
+{
+    return first + second * third / fourth;
+}
