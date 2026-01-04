cbdd6e76a072a4a745f6b46d816a11169d42da90
McLoyf <alex.ortiz242005@gmail.com>
2026-01-03 19:07:29 -0600

    Complete 2.3

--- Learn C++/Chapter_2/2.3.cpp
+++ Learn C++/Chapter_2/2.3.cpp
@@ -1,1 +1,19 @@
 // Void functions (non-value returning functions)
+
+#include <iostream>
+
+void printHi()
+{
+    std::cout << "Hi\n";
+    // return 5;            // Compile error: trying to return a value in a void function
+}
+
+int main()
+{
+    printHi();              // Works perfectly fine
+
+    // Compile error as the console is trying to print the return value of printHi() which obviously doesn't exist
+    // std::cout << printHi();
+
+    return 0;
+}
