0c9871e7ce32e93d5e81967d8545227b8dca5681
McLoyf <alex.ortiz242005@gmail.com>
2026-01-04 13:55:09 -0600

    Complete 2.5

--- Learn C++/Chapter_2/2.5.cpp
+++ Learn C++/Chapter_2/2.5.cpp
@@ -1,1 +1,20 @@
 // Introduction to local scope
+
+#include <iostream>
+
+int getValueFromUser()
+{
+    int input;          // input is a local variable and cannot be used in main
+    std::cout << "Enter an integer: ";
+    std::cin >> input;
+    return input;
+}
+
+int main()
+{
+    int num{ getValueFromUser() };
+    
+    std::cout << "You entered " << num << '\n';
+
+    return 0;
+}
