cff13152f15f0bc532d00bb1b581b4a8a8f1b2af
McLoyf <alex.ortiz242005@gmail.com>
2026-01-06 10:18:58 -0600

    Complete 2.9.cpp

--- Learn C++/Chapter_2/2.9.cpp
+++ Learn C++/Chapter_2/2.9.cpp
@@ -1,1 +1,15 @@
 // Naming collisions and an introduction to namespaces
+
+#include <iostream>
+
+// All of the following statements are part of the global namespace
+
+void foo();
+int x;
+int y{ 5 };
+//x = 5;    Compile error: executable statements are not allowed in namespaces
+
+int main()
+{
+    std::cout << "Hello world!"; // when we say cout, we mean the cout defined in the std namespace
+}
