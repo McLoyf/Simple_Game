79d8a6974e5667e1d60dae8498c19be970a62700
McLoyf <alex.ortiz242005@gmail.com>
2026-01-09 13:06:38 -0600

    Complete 4.2

--- Learn C++/Chapter_4/4.2/4.2.cpp
+++ Learn C++/Chapter_4/4.2/4.2.cpp
@@ -1,1 +1,34 @@
 // Void
+# include <iostream>
+
+void value; // won't work, variables can't be defined with incomplete type void
+
+void doNothing()    // void means that it lacks a return value and in this case does nothing
+{
+    return;
+}
+
+int getValue(void){
+    int x;
+    std::cin >> x;
+
+    return x;
+}
+
+int main()
+{
+    doNothing();
+    std::cout << "doNothing() has been called." << '\n';
+    return 0;
+}
+
+/* Deprecated: Functions that don't take parameters
+*  The function getValue() (seen above) will compile in C++ to be backwards compatiible but
+*  the use of keyword void is considered deprecated in C++, below is the preferred way to
+*  say that a function has no parameters
+*
+*  int getValue()
+*  {
+*     (code goes here)
+*  }
+*/
