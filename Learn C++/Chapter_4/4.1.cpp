e4625d6a81988a725e7872c9b22bede86749e531
McLoyf <alex.ortiz242005@gmail.com>
2026-01-09 12:24:39 -0600

    Create 4.1 folder and 4.1.cpp
    Complete 4.1.cpp

--- Learn C++/Chapter_4/4.1.cpp
+++
@@ -1,1 +0,0 @@
-// Introduction to fundamental data types
---
+++ Learn C++/Chapter_4/4.1/4.1.cpp
@@ -0,0 +1,18 @@
+// Introduction to fundamental data types
+/* Below is a list of the fundamental data types in C++:
+   - float : a number with a fractional part
+   - double : a number with a fractional part (double precision)
+   - long double : a number with a fractional part (extended precision)
+   - bool : true or false
+   - char : single character
+   - wchar_t : wide character
+   - char8_t : 8-bit character
+   - char16_t : 16-bit character
+   - char32_t : 32-bit character
+   - short int : a signed integer type with a range of -32,768 to 32,767
+   - int : a signed integer type with a range of -2,147,483,648 to 2,147,483,647
+   - long int : a signed integer type with a range of -2,147,483,648 to 2,147,483,647
+   - long long int : a signed integer type with a range of -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+   - std::nullptr_t : a null pointer type
+   - void : no value
+*/
