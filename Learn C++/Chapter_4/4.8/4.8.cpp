4e0f7da660eb28a7ff69eee35f0b2bc878402e64
McLoyf <alex.ortiz242005@gmail.com>
2026-01-10 19:26:59 -0600

    Complete 4.8

--- Learn C++/Chapter_4/4.8/4.8.cpp
+++ Learn C++/Chapter_4/4.8/4.8.cpp
@@ -1,1 +1,23 @@
 // Float point numbers
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    std::cout << 5.0 << '\n';
+    std::cout << 6.7f << '\n';
+    std::cout << 9876543.21 << '\n';
+
+    std::cout << std::setprecision(17);
+    std::cout << 3.33333333333333333333333333333333333333f << '\n'; // f suffix means float
+    std::cout << 3.33333333333333333333333333333333333333 << '\n';  // no suffix means double
+
+    return 0;
+}
+
+/*  Precision of non-integer data types
+*   float      : 6-9 digits of precision
+*   double     : 15-18 digits of precision
+*   long double: 15,18, or 33 digits of precision
+*/
