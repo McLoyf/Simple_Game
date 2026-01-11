808c5f76bcbc02bdacef4a2cab5fca416296a308
McLoyf <alex.ortiz242005@gmail.com>
2026-01-10 18:42:03 -0600

    Complete 4.7

--- Learn C++/Chapter_4/4.7/4.7.cpp
+++ Learn C++/Chapter_4/4.7/4.7.cpp
@@ -1,1 +1,17 @@
 // Introduction to scientific notation
+
+/* How to convert decimal numbers to scientific notation
+*  Exponent starts at zero
+*  If your number is an integer, the decimal is impliclitly on the right of the whole number
+*  Slide the decimal place left or right so there is only one non-zero digit to the left or right of decimal
+*     Each place you slide the decimal to the left increases the exponent by 1
+*     Each place you slide the decimal to the right decreases the exponent by 1
+*  Trim off any leading zeros
+*  Trim off any trailing xero only if the orignal number had no decimal point.
+*
+*  Examples:
+*  Start with : 600.410
+*  Slide decimal left 2 spaces: 6.00410e2
+*  No leading zeros to trim: 6.00410e2
+*  Don't trim trailing zeros: 6.00410e2
+*/
